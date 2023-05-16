8937b92e142819a779be27762b760cc9d77fff70
Nikita Trenikhin <nik.trenikhin@gmail.com>
2023-05-16 17:28:38 +0500

    init

--- Assets/Scripts/Abstract/Presenter.cs
+++
@@ -1,6 +0,0 @@
-public abstract class Presenter
-{
-    public abstract void Enable();
-
-    public abstract void Disable();
-}
--- Assets/Scripts/Сonstruction/IBouncy.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public interface IBouncy
-{
-    void Bounce(Vector3 direction);
-}
